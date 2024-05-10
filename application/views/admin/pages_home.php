6a1b373d078f98cfbab6c8397591f0b24f462ab8
rohan0420 <skrohan0420@gmail.com>
2024-05-10 19:17:40 +0530

    added banners input

--- application/views/admin/pages_home.php
+++ application/views/admin/pages_home.php
@@ -1,4 +1,3 @@
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -10,8 +9,98 @@
     <!-- Content Row -->
     <div class="row">
 
+        <div class="col-xl-12 col-lg-12">
 
-   
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h5 class="m-0 font-weight-bold text-primary">Banners</h5>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body row">
+                    <div class="col-xl-6 col-lg-6 card shadow">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Banners Text</h6>
+                        </div>
+                        <div class="card-body">
+                            <form action="<?= base_url('admin/Pages/update_quotes') ?>" method="POST">
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Heading</label>
+                                   <input type="text" class="form-control" name="Heading" value=""
+                                        placeholder="Heading" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">Second Heading</label>
+                                    <input type="text" class="form-control" name="Heading_two" value=""
+                                        placeholder="Second Heading" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">Corner text </label>
+                                    <input type="text" class="form-control" name="corner_text" value=""
+                                        placeholder="corner text " required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">Second Corner text</label>
+                                    <input type="text" class="form-control" name="corner_text_two" value=""
+                                        placeholder="Second Corner text" required>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-success" id="" value="Update">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 card shadow">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                            <h6 class="m-0 font-weight-bold text-primary">Banner Image</h6>
+                        </div>
+                        <div class="card-body">
+                            <form enctype="multipart/form-data" action="" method="POST">
+                                <div class='form-group' id="imagePreview">
+                                    <div>
+                                        <label for="formGroupExampleInput2">Upload image</label>
+                                    </div>
+                                    <img src="" height="100" id="home_banner_img" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="file" class="form-control-file" placeholder="Banner image"
+                                        name="home_banner_img[]" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" hidden value="" name="uid">
+                                    <input type="submit" class="btn btn-success" id="" value="Upload">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h5 class="m-0 font-weight-bold text-primary">All Banners</h5>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-info" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Image</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
 
     </div>
 
