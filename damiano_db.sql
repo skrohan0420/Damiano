9bf798f21320e39f27487ae4ef924c8208b6c4c3
rohan0420 <skrohan0420@gmail.com>
2024-05-16 23:35:38 +0530

    sql file addded

--- damiano_db.sql
+++ damiano_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2024 at 03:42 PM
+-- Generation Time: May 16, 2024 at 08:04 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `features`
+--
+
+CREATE TABLE `features` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `details` text NOT NULL,
+  `img_path` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `features`
+--
+
+INSERT INTO `features` (`id`, `uid`, `product_id`, `title`, `details`, `img_path`, `created_at`) VALUES
+(3, 'FTR706CD22220240515', 'PRDEE94A1EB20240515', 'fdg', 'ghsdfh', '/uploads/feature_img/285e3849a6a28f1e3be9bf74b792b99f.png', '2024-05-15 17:08:19'),
+(6, 'FTRB4B40AA320240515', 'PRDF9C2FF6720240515', 'fdghf', 'ghdfhg', '/uploads/feature_img/7a1cb0daf7db79f5d7288ea182dedf8a.jpg', '2024-05-15 19:00:00'),
+(7, 'FTRD42117A520240515', 'PRDF9C2FF6720240515', 'gfhjg', 'thsdhtfghftghfghgfhfg', '/uploads/feature_img/99f49a9d66fd5471d5b154429c41a407.jpg', '2024-05-15 19:00:17'),
+(8, 'FTR7E39D5A120240516', 'PRDB814E4DF20240516', 'fdgdrgd', 'fsghsdfhgfdsh', '/uploads/feature_img/df61018c046d52b96b160698fca6d954.JPG', '2024-05-16 20:29:18');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `home_about_img`
 --
 
@@ -39,7 +65,7 @@ CREATE TABLE `home_about_img` (
 --
 
 INSERT INTO `home_about_img` (`id`, `uid`, `img_path`, `created_at`) VALUES
-(1, 'IMG_1', '/uploads/home_about_img/e3bac3297d32e4fa28dd40a2ba9b2ef1.jpg', '2024-05-11 18:50:50'),
+(1, 'IMG_1', '/uploads/home_about_img/69bc7a61a7b6c67eeb394733434e4c43.jpg', '2024-05-11 18:50:50'),
 (2, 'IMG_2', '/uploads/home_about_img/1d1799cc7da7976097ef22e1c08d457c.jpg', '2024-05-11 18:51:29'),
 (3, 'IMG_3', '/uploads/home_about_img/aa178c2b8c6ce9715a1ca4dd2b612f97.jpg', '2024-05-11 18:51:29');
 
@@ -83,7 +109,32 @@ CREATE TABLE `home_announcement_file` (
 --
 
 INSERT INTO `home_announcement_file` (`id`, `uid`, `path`, `created_at`) VALUES
-(7, 'BNHU0AFB1F9D20240513', '/uploads/home_announcement_file/b0f52f87b65b4e73161d4ccf4d135369.pdf', '2024-05-13 16:53:28');
+(7, 'BNHU0AFB1F9D20240513', '/uploads/home_announcement_file/b0f52f87b65b4e73161d4ccf4d135369.pdf', '2024-05-13 16:53:28'),
+(8, 'BNHU2D5104C120240516', '/uploads/home_announcement_file/ca28efdf54c57ef7a91ea233179d1245.pdf', '2024-05-16 23:11:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_appreciation_img`
+--
+
+CREATE TABLE `home_appreciation_img` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `img_path` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `home_appreciation_img`
+--
+
+INSERT INTO `home_appreciation_img` (`id`, `uid`, `title`, `img_path`, `created_at`) VALUES
+(3, 'APR4BABA9A920240514', 'img_1', '/uploads/home_appreciation_img/98a40933baff9868195700862ebe4690.jpg', '2024-05-14 12:55:25'),
+(4, 'APRA64630F820240514', 'img_2', '/uploads/home_appreciation_img/ad2d345a85a40a51acdd1d77a384536d.jpg', '2024-05-14 12:55:38'),
+(5, 'APR4D9780D820240514', 'img_3', '/uploads/home_appreciation_img/aac9800f6a602347a845a4bb9df4a579.jpg', '2024-05-14 12:55:49'),
+(6, 'APRCD179A8820240514', 'img_4', '/uploads/home_appreciation_img/d029c9dadd3fd4627c91d7dc21cd60f8.jpg', '2024-05-14 12:56:01');
 
 -- --------------------------------------------------------
 
@@ -150,7 +201,10 @@ CREATE TABLE `home_infrastructure` (
 --
 
 INSERT INTO `home_infrastructure` (`id`, `uid`, `title`, `img_path`, `created_at`) VALUES
-(3, 'INFFE3A0F2C20240513', 'abc', '/uploads/home_infrastructure_img/3630c0ffcec15ce8fdc35d2bd18284ad.jpg', '2024-05-13 17:22:26');
+(4, 'INF1223D20620240514', 'text_1', '/uploads/home_infrastructure_img/46e6cd13cd40e0b7c1f9abf69941bd54.jpg', '2024-05-14 12:56:39'),
+(5, 'INFD32BF47B20240514', 'text_2', '/uploads/home_infrastructure_img/20f0effb01ae696912eded2ecd697554.jpg', '2024-05-14 12:56:51'),
+(6, 'INFA9D0F4DE20240514', 'text_3', '/uploads/home_infrastructure_img/4711572dd254784bd331e60517bfa5db.jpg', '2024-05-14 12:57:01'),
+(7, 'INF1FBD20C420240514', 'text_4', '/uploads/home_infrastructure_img/8223ed58af8d9b541cf7047a1a5b41d6.jpg', '2024-05-14 12:57:14');
 
 -- --------------------------------------------------------
 
@@ -171,6 +225,61 @@ CREATE TABLE `home_updates_img` (
 
 INSERT INTO `home_updates_img` (`id`, `uid`, `img_path`, `created_at`) VALUES
 (1, 'UBNH86967F20240511', '/uploads/home_update_img/670d6df2ac5797fd26b39eb6e18224fa.jpg', '2024-05-13 15:28:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `name` text NOT NULL,
+  `details` text NOT NULL,
+  `img_path` text NOT NULL,
+  `banner_img_path` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `uid`, `name`, `details`, `img_path`, `banner_img_path`, `created_at`) VALUES
+(4, 'PRDEE94A1EB20240515', 'product 1', ' Damiano traces its origin as early as 1922. The company was representing the European companies for their procurement activities in India and was a part of the CTC Group which was a \r\n                                dominant player in the jute industry in India. Damiano underwent restructuring in late 1960\'s and re-established itself in its present form by diversifying into manufacturing in 1968 and has been manufacturing Pole Line Hardware\'s, \r\n                                Steel structures, insulator and Conductor accessories, Clamps and Connectors, Aerial Cable Accessories, Fuse Cutouts and Telecommunications OSP Hardware. The steps were small but firm and the company expanded from cottage scale manufacturer \r\n                                to a large industry with in house facilities for fabrication, forging, machining, wire forming, moulding, die casting, hot dip galvanizing (including centrifuge) and electroplating. The company started exporting from 1972 and has niche presence \r\n                                not only in the domestic market but also across the globe. The company started exporting from 1972 and has niche presence not only in the domestic market but also across the globe. Damiano has also undertaken turnkey project exports of plants to \r\n                                manufacture stranded wires and conductors and other components. Damiano believes in providing its customers competitive prices, standard quality, durable packing, in-house tested products, just in time delivery and flexibility in operations. \r\n                                It prides itself for its professionalism and focus on quality and believes that with the support of its customers it can successfully exceed its present annual capacity of 40000 tonnes.', '/uploads/product_img/f1a44977a8144176f0813d57dd494c69.jpg', '/uploads/product_banner_img/2c8fd3485b563ccd9d021f2e57cf83f0.jpg', '2024-05-15 15:38:09'),
+(5, 'PRDF9C2FF6720240515', 'product_2', ' Damiano traces its origin as early as 1922. The company was representing the European companies for their procurement activities in India and was a part of the CTC Group which was a \r\n                                dominant player in the jute industry in India. Damiano underwent restructuring in late 1960\'s and re-established itself in its present form by diversifying into manufacturing in 1968 and has been manufacturing Pole Line Hardware\'s, \r\n                                Steel structures, insulator and Conductor accessories, Clamps and Connectors, Aerial Cable Accessories, Fuse Cutouts and Telecommunications OSP Hardware. The steps were small but firm and the company expanded from cottage scale manufacturer \r\n                                to a large industry with in house facilities for fabrication, forging, machining, wire forming, moulding, die casting, hot dip galvanizing (including centrifuge) and electroplating. The company started exporting from 1972 and has niche presence \r\n                                not only in the domestic market but also across the globe. The company started exporting from 1972 and has niche presence not only in the domestic market but also across the globe. Damiano has also undertaken turnkey project exports of plants to \r\n                                manufacture stranded wires and conductors and other components. Damiano believes in providing its customers competitive prices, standard quality, durable packing, in-house tested products, just in time delivery and flexibility in operations. \r\n                                It prides itself for its professionalism and focus on quality and believes that with the support of its customers it can successfully exceed its present annual capacity of 40000 tonnes.', '/uploads/product_img/fe21fec8dcc2a3cd5feeb3d5a8bc3ce6.jpg', '/uploads/product_banner_img/084188e098bbc4379443c69b0f5fabbd.jpg', '2024-05-15 17:49:54'),
+(7, 'PRD6C8820B820240516', 'fghdhg', 'gfhsfh', '/uploads/product_img/', '/uploads/product_banner_img/', '2024-05-16 18:27:18'),
+(8, 'PRD482FF27F20240516', 'trst', 'gfdh', '', '', '2024-05-16 18:47:35'),
+(9, 'PRDB814E4DF20240516', '123456', 'gdryhdha', '', '', '2024-05-16 19:44:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_banner_img`
+--
+
+CREATE TABLE `product_banner_img` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `img_path` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_banner_img`
+--
+
+INSERT INTO `product_banner_img` (`id`, `uid`, `product_id`, `img_path`, `created_at`) VALUES
+(1, 'PRBNB1F441B920240516', 'PRD482FF27F20240516', '/uploads/product_banner_img/b0891c2ad8ddb46c94ecbd4d516abeb6.png', '2024-05-16 18:47:35'),
+(2, 'PRBN9641D18520240516', 'PRD482FF27F20240516', '/uploads/product_banner_img/d580f7cfb7cd4880c8f9898890fd0680.png', '2024-05-16 18:47:35'),
+(3, 'PRBNDE41F70820240516', 'PRD482FF27F20240516', '/uploads/product_banner_img/5765d4ad022d1ea3c5a6900d990700fc.png', '2024-05-16 18:47:35'),
+(4, 'PRBN7800443720240516', 'PRD482FF27F20240516', '/uploads/product_banner_img/b363796434c7b26d416f97ac3dd0d46b.png', '2024-05-16 18:47:35'),
+(5, 'PRBNF4F0BB6120240516', 'PRD482FF27F20240516', '/uploads/product_banner_img/ce58d06078a77dfc78c51a1024addd21.png', '2024-05-16 18:47:35'),
+(6, 'PRBNDA2D219020240516', 'PRD482FF27F20240516', '/uploads/product_banner_img/0c6cfee2d6dc1f34a51800c3b7a5744d.png', '2024-05-16 18:47:35'),
+(7, 'PRBNBBD569FE20240516', 'PRDB814E4DF20240516', '/uploads/product_banner_img/11affdb384cebf3d4677764c37337893.jpg', '2024-05-16 19:44:32'),
+(8, 'PRBNFB0F45F620240516', 'PRDB814E4DF20240516', '/uploads/product_banner_img/af962c303218672779cd7e8528b707b9.JPG', '2024-05-16 19:44:32');
 
 -- --------------------------------------------------------
 
@@ -218,11 +327,19 @@ CREATE TABLE `user_message` (
 --
 
 INSERT INTO `user_message` (`id`, `uid`, `name`, `email`, `phone`, `subject`, `message`, `created_at`, `updated_at`) VALUES
-(4, 'MSG4D2D332020240513', 'fvgdg fgdsgd', 'admin@gmail.com', '43554', '', 'fhdshg', '2024-05-13 19:06:30', '2024-05-13 19:06:30');
+(4, 'MSG4D2D332020240513', 'fvgdg fgdsgd', 'admin@gmail.com', '43554', '', 'fhdshg', '2024-05-13 19:06:30', '2024-05-13 19:06:30'),
+(5, 'MSGBA0FDC3020240514', ' ', '', '', '', '', '2024-05-14 13:06:30', '2024-05-14 13:06:30'),
+(6, 'MSGB41BE60C20240514', 'fvgdg fgdsgd', 'admin@gmail.com', '1234567', '', 'fghdhghdfh', '2024-05-14 15:14:29', '2024-05-14 15:14:29');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `features`
+--
+ALTER TABLE `features`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `home_about_img`
@@ -240,6 +357,12 @@ ALTER TABLE `home_about_text`
 -- Indexes for table `home_announcement_file`
 --
 ALTER TABLE `home_announcement_file`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `home_appreciation_img`
+--
+ALTER TABLE `home_appreciation_img`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -267,6 +390,18 @@ ALTER TABLE `home_updates_img`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_banner_img`
+--
+ALTER TABLE `product_banner_img`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -281,6 +416,12 @@ ALTER TABLE `user_message`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `features`
+--
+ALTER TABLE `features`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `home_about_img`
@@ -298,7 +439,13 @@ ALTER TABLE `home_about_text`
 -- AUTO_INCREMENT for table `home_announcement_file`
 --
 ALTER TABLE `home_announcement_file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `home_appreciation_img`
+--
+ALTER TABLE `home_appreciation_img`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `home_banner_img`
@@ -316,13 +463,25 @@ ALTER TABLE `home_banner_text`
 -- AUTO_INCREMENT for table `home_infrastructure`
 --
 ALTER TABLE `home_infrastructure`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `home_updates_img`
 --
 ALTER TABLE `home_updates_img`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `product_banner_img`
+--
+ALTER TABLE `product_banner_img`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -334,7 +493,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `user_message`
 --
 ALTER TABLE `user_message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
